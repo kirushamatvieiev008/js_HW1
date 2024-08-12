e64a47873d709dccda6b55db04ce396083f34d23
Кірюша <mkiril087@gmail.com>
2024-08-12 12:15:34 +0300

    gh

---
+++ index1.js
@@ -0,0 +1,12 @@
+let age = 16;
+let isStudent = true;
+let myString = "те що нас не вбиває робить нас сильнішими";
+let myNumber = 37;
+myNumber = 37 + 10;
+let myNull = null;
+let name = "Kiril";
+const userName = prompt(`введіть ваше ім'я`);
+alert(`вітаємо вас ${userName}`);
+// let all = age isStudent myString 
+console.log(`${age}, ${name} ${isStudent}, ${myString}, ${myNumber}, ${myNull}`);
+
